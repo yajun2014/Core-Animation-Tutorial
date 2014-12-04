01117c07177897a0214de250f03e9ae08ec7178e
yajun <yajun@CEAT-ITSs-iMac.local>
2014-12-03 19:37:55 -0600

    ADD reflection view and function

--- Core Animation Tutorial/BasicAnimationContorller.m
+++ Core Animation Tutorial/BasicAnimationContorller.m
@@ -24,8 +24,8 @@
     self.basicAnimationList = [NSMutableArray arrayWithCapacity:5];
     self.detailAnimationList = [NSMutableArray arrayWithCapacity:5];
     
-    NSArray *myArray =@[@"Simple Animation",@"Implicit Animation",@"picturs"];
-    NSArray *myDetailArray =@[@"Rotate,Scale,Move and Combine Animation",@"Change position, opacity,color,size and corner",@"picturs"];
+    NSArray *myArray =@[@"Simple Animation",@"Implicit Animation",@"Reflection"];
+    NSArray *myDetailArray =@[@"Rotate,Scale,Move and Combine Animation",@"Change position, opacity,color,size and corner",@"Show Great Wall reflection"];
     
     self.basicAnimationList = [NSMutableArray arrayWithArray:myArray];
     self.detailAnimationList = [NSMutableArray arrayWithArray:myDetailArray];
--- Core Animation Tutorial/ReflectionVC.m
+++ Core Animation Tutorial/ReflectionVC.m
@@ -16,44 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
-    UIImage *image = [UIImage imageNamed:@"Great Wall.png"];
+    UIImage *myImage = [UIImage imageNamed:@"Great Wall.png"];
+    // add layer whose content is a picture
+    CALayer *myLayer = [CALayer layer];
+    myLayer.contents = (id)myImage.CGImage;
+    myLayer.bounds = CGRectMake(0, 0, myImage.size.width, myImage.size.height);
+    myLayer.position = CGPointMake(160, 180);
+    myLayer.borderColor = [UIColor redColor].CGColor;
+    myLayer.borderWidth = 1.0;
+    [self.view.layer addSublayer:myLayer];
     
-    // Image Layer
-    CALayer *imageLayer = [CALayer layer];
-    imageLayer.contents = (id)image.CGImage;
-    imageLayer.bounds = CGRectMake(0, 0, image.size.width, image.size.height);
-    imageLayer.position = CGPointMake(160, 130);
-    imageLayer.borderColor = [UIColor darkGrayColor].CGColor;
-    imageLayer.borderWidth = 1.0;
-    [self.view.layer addSublayer:imageLayer];
+    // add reflection layer
+    CALayer *myReflectLayer = [CALayer layer];
+    myReflectLayer.contents = myLayer.contents;
+    myReflectLayer.bounds = myLayer.bounds;
+    myReflectLayer.position = CGPointMake(160, 371);
+    myReflectLayer.borderColor = myLayer.borderColor;
+    myReflectLayer.borderWidth = myLayer.borderWidth;
+    myReflectLayer.opacity = 0.6;
     
-    // Reflection Layer
-    CALayer *reflectionLayer = [CALayer layer];
-    reflectionLayer.contents = imageLayer.contents;
-    reflectionLayer.bounds = imageLayer.bounds;
-    reflectionLayer.position = CGPointMake(160, 330);
-    reflectionLayer.borderColor = imageLayer.borderColor;
-    reflectionLayer.borderWidth = imageLayer.borderWidth;
-    reflectionLayer.opacity = 0.7;
-    // Transform X by 180 degrees
-    [reflectionLayer setValue:[NSNumber numberWithFloat:M_PI] forKeyPath:@"transform.rotation.x"];
+    [myReflectLayer setValue:[NSNumber numberWithFloat:M_PI] forKeyPath:@"transform.rotation.x"];
     
-    // Gradient Layer - Use as mask
+    // add Gradient Layer as mask
     CAGradientLayer *gradientLayer = [CAGradientLayer layer];
-    gradientLayer.bounds = reflectionLayer.bounds;
-    gradientLayer.position = CGPointMake(reflectionLayer.bounds.size.width / 2, reflectionLayer.bounds.size.height * 0.65);
+    gradientLayer.bounds = myReflectLayer.bounds;
+    gradientLayer.position = CGPointMake(myReflectLayer.bounds.size.width / 2, myReflectLayer.bounds.size.height * 0.65);
     gradientLayer.colors = [NSArray arrayWithObjects:(id)[[UIColor clearColor] CGColor],(id)[[UIColor whiteColor] CGColor], nil];
-    gradientLayer.startPoint = CGPointMake(0.5, 0.5);
-    gradientLayer.endPoint = CGPointMake(0.5, 1.0);
-    
-    // Add gradient layer as a mask
-    reflectionLayer.mask = gradientLayer;
-    [self.view.layer addSublayer:reflectionLayer];
+    gradientLayer.startPoint = CGPointMake(0.5, 0.0);
+    gradientLayer.endPoint = CGPointMake(0.5, 1.5);
+    myReflectLayer.mask = gradientLayer;
+    [self.view.layer addSublayer:myReflectLayer];
 
-    
-    
     
 }
 
